255f98d448df45db0e892fb188938105563dd0db
Maanu <86370615+Maanucoder@users.noreply.github.com>
2021-09-04 20:18:28 +0530

    Update and rename table mistake finder .py to Code.py

--- Code.py
+++ Code.py
@@ -1,14 +1,15 @@
-
+#function
 def iscorrect(Number,Table):
 	i=1
-	for item in Table:
+	for item in Table
+#To check that table correct or not.
 		if item==Number*i:
 			i=i+1
+#such that table only gave 10 multiple
 		elif  i>10:
 			break
+#if function find any mistake
 		else:
 			print("he is frood")
 			print(f"he cheat, {item} is wrong, correct is {Number*i}")
 			i=i+1
-
-iscorrect(7,[7,23,14,54,68,35,35,8,4])
